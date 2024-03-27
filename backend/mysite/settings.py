b4c0c003a728a5566c1ebbf8fe3fb79f06c653fb
yyysolhhh <solbaram37@gmail.com>
2024-03-28 01:39:25 +0900

    Chore: add LOGGING to settings.py
    query log 확인하기 위한 세팅

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -133,3 +133,21 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django.db.backends": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+        },
+    },
+}
