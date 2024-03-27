fb5e37b728fda658bf7b369f380fe4b9b36a1416
yyysolhhh <solbaram37@gmail.com>
2024-03-28 01:30:42 +0900

    Chore: install djangorestframework
    - drf 설치
    - APPS에 rest_framework 추가

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -111,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Seoul"
 
 USE_I18N = True
 
