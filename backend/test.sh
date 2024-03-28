a1987b8728f1c1d92efa7b931fbbf853d0c05aba
yyysolhhh <solbaram37@gmail.com>
2024-03-29 02:56:02 +0900

    Chore: add dockerfile, docker-compose, nginx.conf, test.sh
    - 도커관련 파일 추가
    - .gitignore 수정

---
+++ backend/test.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+COLOR_GREEN=`tput setaf 2;`
+COLOR_NC=`tput sgr0;` # No Color
+
+echo "Starting black"
+poetry run black .
+echo "OK"
+
+echo "Starting isort"
+poetry run isort .
+echo "OK"
+
+echo "Starting mypy"
+poetry run mypy .
+echo "OK"
+
+echo "Starting pytest with coverage"
+poetry run coverage run -m pytest
+poetry run coverage report -m
+poetry run coverage html
+
+echo "${COLOR_GREEN}All tests passed successfully!${COLOR_NC}"
