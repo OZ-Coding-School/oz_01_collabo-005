87222a0057e1514a1633390c7a33b50e96464bf0
yyysolhhh <solbaram37@gmail.com>
2024-03-28 16:21:30 +0900

    Feat: set database
    - database 설정
    - mysqlclient 설치

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": env("DB_ENGINE"),
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "HOST": env("DB_HOST"),
+        "PORT": env("DB_PORT")
     }
 }
 
