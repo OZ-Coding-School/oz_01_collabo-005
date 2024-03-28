703be7a16069e0d08619dcdc60831fc1e461aecb
yyysolhhh <solbaram37@gmail.com>
2024-03-28 19:07:21 +0900

    Feat: add custom user model to users
    - User 모델 클래스 추가
    - settings에 등록
    - UserManager 작성 전

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -146,6 +146,8 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+AUTH_USER_MODEL = 'users.User'
+
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema"
 }
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin, AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self):
+        pass
+
+    def create_superuser(self):
+        pass
+
+
+class User(AbstractBaseUser, PermissionsMixin, AbstractUser):
+    nickname = models.CharField(max_length=10)
+    password = models.CharField(max_length=50)
+    nationality = models.CharField(max_length=50)
+    name = models.CharField(max_length=10)
+    phone = models.CharField(max_length=30)
+    email = models.EmailField()
+    birthday = models.DateField()
+    profession = models.CharField(max_length=10)
+
+    USERNAME_FIELD = 'nickname'
+    objects = UserManager()
