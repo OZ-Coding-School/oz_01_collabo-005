67a19578783bfcf88cdf095b59348ce37d27c8d0
yyysolhhh <solbaram37@gmail.com>
2024-03-29 18:03:09 +0900

    Chore: docker compose success
    update docker-compose.yml, entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-exec python manage.py runserver
+#exec python manage.py migrate
+#exec python manage.py runserver 0.0.0.0:8000
+python manage.py migrate
+python manage.py runserver 0.0.0.0:8000
--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS: List[str] = []
+ALLOWED_HOSTS: List[str] = ["0.0.0.0"]
 
 
 # Application definition
