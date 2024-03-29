2c7a9f5ee31f70d91327d364068fedd67ded31ca
minhoshin11 <tlsalsgh777@naver.com>
2024-03-29 11:32:19 +0900

    Feat: 초기설정
    npm install react-route-dom , axios , bootstrap

---
+++ frontend/src/pages/myinfo/MyInfo.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MyInfo() {
+  return (
+    <div>MyInfo</div>
+  )
+}
+
+export default MyInfo
---
+++ frontend/src/pages/notice/Notice.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Notice() {
+  return (
+    <div>Notice</div>
+  )
+}
+
+export default Notice
