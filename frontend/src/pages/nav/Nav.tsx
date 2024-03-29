e49251547843e78982742a7f92a49f545ea5ab11
choitoady <healutoday@naver.com>
2024-03-29 19:38:13 +0900

    Feat: navbar 레이아웃
    navbar 부분 레이아웃 잡음

--- frontend/src/pages/nav/Nav.tsx
+++ frontend/src/pages/nav/Nav.tsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { FiSearch } from "react-icons/fi";
+import { SlMenu } from "react-icons/sl";
+import "./Nav.css";
 
 function Nav() {
   return (
-    <div className="Navar-container">
-      <div className="Logo"></div>
+    <div className="Nav-container">
+      <div className="header-box">
+        <div className="Logo">LOGO</div>
+        <div className="navbar-button-box">
+          <button className="search-button">
+            <FiSearch size={30} />
+          </button>{" "}
+          <button onClick={() => console.log("버튼")} className="menu-button">
+            <SlMenu size={30} />
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
