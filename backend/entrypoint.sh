6a7b710a7942adf100506787ec62eabf1d032fa1
yyysolhhh <solbaram37@gmail.com>
2024-03-30 13:45:06 +0900

    Chore: install gunicorn
    - gunicorn 설치
    - entrypoint.sh에서 gunicorn으로 실행

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,4 +3,5 @@
 #exec python manage.py migrate
 #exec python manage.py runserver 0.0.0.0:8000
 python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
+#python manage.py runserver 0.0.0.0:8000
+gunicorn --bind 0:8000 mysite.wsgi:application
