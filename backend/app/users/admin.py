16ca6d14d41cf3fbbe493e9b0895eacdb29d2abd
yyysolhhh <solbaram37@gmail.com>
2024-04-01 17:54:37 +0900

    Feat: update user admin
    user admin 페이지 설정 추가

--- backend/app/users/admin.py
+++ backend/app/users/admin.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from app.users.models import User
 
-admin.site.register(User)
+# admin.site.register(User)
 
-# @admin.register(User)
-# class CustomUserAdmin(UserAdmin):
-#     pass
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ("email", "nickname", "is_staff", "is_active")
+    fieldsets = (
+        (None, {"fields": ("email", "nickname", "is_staff", "is_active", "date_joined")}),
+        ("Personal Info", {"fields": ("first_name", "last_name", "phone", "nationality", "date_of_birth", "profession")})
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide", ),
+            "fields": (
+                "email", "nickname",
+            )
+        })
+    )
+    ordering = ("email", "nickname", "is_staff", "is_active")
