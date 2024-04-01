c3da4f339eaba127344a04fa62fc90b48b6218b2
yyysolhhh <solbaram37@gmail.com>
2024-04-02 03:05:19 +0900

    Fix: fix swagger error
    fix "Failed to load API definition." error

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -169,14 +169,12 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "user.User"
 
 REST_FRAMEWORK = {
-    "DEFAULT_SCHEMA_CLASSES": (
-        "drf_spectacular.openapi.AutoSchema",
-    ),
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_PERMISSION_CLASSES": (
         "rest_framework.permissions.IsAuthenticated",
         # "rest_framework.permissions.IsAdminUser"
     ),
-    'DEFAULT_AUTHENTICATION_CLASSES': (
+    "DEFAULT_AUTHENTICATION_CLASSES": (
         # "rest_framework_simplejwt.authentication.JWTAuthentication",
         "rest_framework.authentication.SessionAuthentication",
         "dj_rest_auth.jwt_auth.JWTCookieAuthentication",
