a5b79497b8a17f7fa502882bedb72fe95d011126
yyysolhhh <solbaram37@gmail.com>
2024-04-02 17:41:44 +0900

    Feat: add google_login, google_callback to views.py
    - social login 관련 뷰 추가
    - url 수정

--- backend/app/user/urls.py
+++ backend/app/user/urls.py
@@ -3,8 +3,7 @@ from django.contrib.auth import views as auth_views
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenRefreshView
 
-from app.user import views
-from app.user.views import UserViewSet
+from app.user.views import UserViewSet, google_login, google_callback, GoogleLogin
 
 # from app.user.views import UserRegisterAPIView, UserLoginAPIView
 
@@ -14,7 +13,10 @@ router.register(r"", UserViewSet, basename='user')
 
 urlpatterns = [
     path("", include(router.urls)),
-    path("google", views.GoogleLogin.as_view(), name="google-login")
+    path("google/login/", google_login, name="google-login"),
+    path("google/callback/", google_callback, name="google-callback"),
+    path('google/login/finish', GoogleLogin.as_view(), name="google-login-todjango")
+    # path("google/", views.GoogleLogin.as_view(), name="google")
     # path("sign-up/", UserRegisterAPIView.as_view(), name="sign-up"),
     # path("sign-in/", UserLoginAPIView.as_view(), name="sign-in"),
     # path("refresh/", TokenRefreshView.as_view(), name="refresh"),
@@ -22,7 +24,4 @@ urlpatterns = [
     # path("", UserView.as_view(), name="user"),
     # path("login/", auth_views.LoginView.as_view(), name="login"),
     # path("logout/", auth_views.LogoutView.as_view(), name="logout"),
-    # path("google/login/", views.google_login, name="google_login"),
-    # path("google/callback/", views.google_callback, name="google_callback"),
-    # path('google/login/finish', views.GoogleLogin.as_view(), name="google_login_godjango")
 ]
--- backend/app/user/views.py
+++ backend/app/user/views.py
@@ -1,9 +1,14 @@
+import requests
+from allauth.socialaccount.models import SocialAccount
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from dj_rest_auth.registration.serializers import SocialLoginSerializer
 from dj_rest_auth.registration.views import SocialLoginView
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import render, redirect
+from json import JSONDecodeError
 from rest_framework import viewsets
+from rest_framework.decorators import api_view
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -13,10 +18,15 @@ from rest_framework_simplejwt.tokens import Token
 
 from app.user.models import User
 from app.user.serializers import UserSerializer
+from mysite.settings import env
+
 # from app.user.serializers import UserRegisterSerializer, UserLoginSerializer, UserSerializer
 
-BASE_URL = "http://localhost:8000/accout/rest-auth/"
+BASE_URL = "http://localhost:8000/api/account/"
+# GOOGLE_CALLBACK_URI = BASE_URL
+# GOOGLE_CALLBACK_URI = BASE_URL + "google/callback/"
 GOOGLE_CALLBACK_URI = BASE_URL + "google/callback/"
+state = env("STATE")
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -30,6 +40,56 @@ class GoogleLogin(SocialLoginView):
     client_class = OAuth2Client
     serializer_class = SocialLoginSerializer
 
+
+# @api_view(['POST'])
+def google_login(request):
+    scope = "https://www.googleapis.com/auth/userinfo.email"
+    client_id = env("SOCIAL_AUTH_GOOGLE_CLIENT_ID")
+    return redirect(f"https://accounts.google.com/o/oauth2/v2/auth?client_id={client_id}&response_type=code&redirect_uri={GOOGLE_CALLBACK_URI}&scope={scope}")
+
+
+# @api_view(["GET"])
+def google_callback(request):
+    client_id = env("SOCIAL_AUTH_GOOGLE_CLIENT_ID")
+    client_secret = env("SOCIAL_AUTH_GOOGLE_SECRET")
+    code = request.GET.get("code")
+
+    # access token 요청
+    token_request = requests.post(f"https://oauth2.googleapis.com/token?client_id={client_id}&client_secret={client_secret}&code={code}&grant_type=authorization_code&redirect_uri={GOOGLE_CALLBACK_URI}&state={state}")
+    token_req_json = token_request.json()
+    error = token_req_json.get("error")
+    if error is not None:
+        raise JSONDecodeError(error)
+    access_token = token_req_json.get("access_token")
+
+    # access token으로 이메일값 요청
+    email_request = requests.get(f"https://accounts.google.com/o/oauth2")
+    if email_request.status_code != 200:
+        return JsonResponse({"error": "Failed to get email"}, status=status.HTTP_400_BAD_REQUEST)
+    email_req_json = email_request.json()
+    email = email_req_json.get("email")
+
+    # 받은 이메일, access_token, code로 회원가입/로그인
+    try:
+        user = User.objects.get(email=email)
+        # 해당 이메일 유저 찾기
+        social_user = SocialAccount.objects.get(user=user)
+        if social_user.provider != "google":
+            return JsonResponse({"error": "No matching social type"}, status=status.HTTP_400_BAD_REQUEST)
+
+        data = {"access_token": access_token, "code": code}
+        accept = requests.post(f"{BASE_URL}google/login/finish", data=data)
+        accept_status = accept.status_code
+        if accept_status != 200:
+            return JsonResponse({"error": "Failed to signup"}, status=accept_status)
+
+        accept_json = accept.json()
+        accept_json.pop('user', None)
+        return JsonResponse(accept_json)
+
+    except SocialAccount.DoesNotExist:
+        # user는 있는데 SocialAccount가 없을때
+        return JsonResponse({"error": "Email exists but not social user"}, status=status.HTTP_400_BAD_REQUEST)
 
 # class UserRegisterAPIView(APIView):
 #     def post(self, request: Request):
