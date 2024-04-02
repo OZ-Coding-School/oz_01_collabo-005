034783a9fa4271ba5091710c687369ae74f3a938
yyysolhhh <solbaram37@gmail.com>
2024-04-03 03:58:05 +0900

    Feat: add BaseModel, community or club model
    - community나 club중 하나만 남기기
    - common에 BaseModel 추가

--- backend/app/community/models.py
+++ backend/app/community/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from app.common.models import BaseModel
+
+
+class Category(BaseModel):
+    name = models.CharField(max_length=10, unique=True)
+    picture = models.ImageField(upload_to='')
+
+
+class Community(BaseModel):
+    name = models.CharField(max_length=10, unique=True)
+    description = models.TextField()
+    category = models.ForeignKey("Category", on_delete=models.SET_NULL, null=True)
+    image = models.ImageField(upload_to='', blank=True, null=True)
+    leader = models.ForeignKey("user.User", on_delete=models.SET_NULL, null=True)
+    max_members = models.IntegerField(default=100)
+    place = models.CharField(max_length=10, null=True, blank=True, help_text="frequent gathering place")
