345db99dc6bd19675b2f47c657e99d184016e337
yyysolhhh <solbaram37@gmail.com>
2024-04-04 11:44:44 +0900

    Feat: add category, comment, activity app
    - startapp category
    - startapp comment
    - startapp activity

---
+++ backend/app/category/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+from app.common.models import BaseModel
+
+
+class Category(BaseModel):
+    name = models.CharField(max_length=10, unique=True)
+    picture = models.ImageField(upload_to='')
+
