2e94a10faccb515378d65916d278b4a3d6c861d9
yyysolhhh <solbaram37@gmail.com>
2024-04-05 10:41:09 +0900

    Feat: update category ModelViewSet to ReadOnlyModelViewSet
    - category viewset 변경
    - category serializer HyperlinkedModelSerializer로 변경

--- backend/app/category/serializers.py
+++ backend/app/category/serializers.py
@@ -3,8 +3,10 @@ from rest_framework import serializers
 from app.category.models import Category
 
 
-# class CategorySerializer(serializers.HyperlinkedModelSerializer):
-class CategorySerializer(serializers.ModelSerializer):
+# class CategorySerializer(serializers.ModelSerializer):
+class CategorySerializer(serializers.HyperlinkedModelSerializer):
+    clubs = serializers.HyperlinkedRelatedField(many=True, view_name='clubs included in category', read_only=True)
+
     class Meta:
         model = Category
-        fields = ['name', 'picture']
+        fields = ["name", "picture", "clubs"]
--- backend/app/category/views.py
+++ backend/app/category/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 
 from app.category.models import Category
+from app.category.permissions import IsAdminOrReadOnly
 from app.category.serializers import CategorySerializer
 
 
-class CategoryViewSet(viewsets.ModelViewSet):
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+
+# class CategoryViewSet(viewsets.ModelViewSet):
+#     queryset = Category.objects.all()
+#     serializer_class = CategorySerializer
+#     permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+#                           IsAdminOrReadOnly]
