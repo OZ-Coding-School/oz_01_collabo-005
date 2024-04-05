51fc61422f501f042be06f72613a183c872d1435
yyysolhhh <solbaram37@gmail.com>
2024-04-05 14:31:50 +0900

    Feat: update category serializers.py, models.py
    - model에 verbose_name, verbose_name_plural 추가
    - serializer에 clubset 추가

--- backend/app/category/models.py
+++ backend/app/category/models.py
@@ -6,3 +6,7 @@ from app.common.models import BaseModel
 class Category(BaseModel):
     name = models.CharField(max_length=10, unique=True)
     picture = models.ImageField(upload_to='images/category/')
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
