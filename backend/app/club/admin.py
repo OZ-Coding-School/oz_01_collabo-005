ba1da9ae993bc7cb65afb1f29225e6a598d723da
yyysolhhh <solbaram37@gmail.com>
2024-04-05 12:53:24 +0900

    Feat: add category, club admin page
    - admin.py에 등록

--- backend/app/club/admin.py
+++ backend/app/club/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from app.club.models import Club
+
+# admin.site.register(Club)
+
+
+@admin.register(Club)
+class ClubAdmin(admin.ModelAdmin):
+    list_display = ("name", "get_category_name", "leader", "max_members")
+
+    def get_category_name(self, obj):
+        return obj.category.name
+
+
+# class CategoryInline(admin.TabularInline):
+#     model = Category
+#     fields = ['name']
