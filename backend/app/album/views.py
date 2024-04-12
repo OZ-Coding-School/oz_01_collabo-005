965223cbbdd56b359d106248c3afe86d7afe2074
yyysolhhh <solbaram37@gmail.com>
2024-04-12 19:02:21 +0900

    Feat: add permissions to album app

--- backend/app/album/views.py
+++ backend/app/album/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 
 from app.album.models import Album
+from app.album.permissions import IsUploaderOrReadOnly
+from app.album.serializers import AlbumSerializer
+from app.board.permissions import IsWriterOrReadOnly
 
 
 class AlbumView(viewsets.ModelViewSet):
     queryset = Album.objects.all()
-    serializer_class =
+    serializer_class = AlbumSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsUploaderOrReadOnly]
