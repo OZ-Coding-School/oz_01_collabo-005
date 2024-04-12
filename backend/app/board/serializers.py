e80809c376748170fac4d52aa48a7f575b782931
yyysolhhh <solbaram37@gmail.com>
2024-04-12 13:50:52 +0900

    Feat: add get_queryset, perform_create to PostViewSet, ScheduleViewSet
    - update USER_TZ to False
    - club, writer 자동으로 입력되게 함

--- backend/app/board/serializers.py
+++ backend/app/board/serializers.py
@@ -12,10 +12,19 @@ from app.board.models import Post, Schedule
 class PostSerializer(serializers.ModelSerializer[Post]):
     class Meta:
         model = Post
-        fields = "__all__"
+        fields = ("title", "content")
+
+    # def create(self, validated_data):
+    #     # validated_data["club"] = self.context["club_id"]
+    #     # validated_data["writer"] = self.context["request"].user
+    #     # return super().create(validated_data)
+    #     club_id = self.context.get("club_id")
+    #     writer = self.context.get("request").user
+    #     post = Post.objects.create(club_id=club_id, writer=writer, **validated_data)
+    #     return post
 
 
 class ScheduleSerializer(serializers.ModelSerializer[Schedule]):
     class Meta:
         model = Schedule
-        fields = "__all__"
+        fields = ("title", "content", "event_time", "place", "max_attendees")
--- backend/app/board/views.py
+++ backend/app/board/views.py
@@ -1,15 +1,34 @@
 from django.shortcuts import render
 from rest_framework import permissions, viewsets
+from rest_framework.exceptions import NotFound
 
 from app.board.models import Post, Schedule
 from app.board.permissions import IsWriterOrReadOnly
 from app.board.serializers import PostSerializer, ScheduleSerializer
+from app.club.models import Club
 
 
 class PostViewSet(viewsets.ModelViewSet[Post]):
-    queryset = Post.objects.all()
+    # queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsWriterOrReadOnly]
+
+    def get_queryset(self):
+        club_id = self.kwargs.get("club_id")
+        return Post.objects.filter(club_id=club_id)
+
+    # def get_object(self):
+    #     club_id = self.kwargs.get("club_id")
+    #     try:
+    #         club = Club.objects.get(id=club_id)
+    #         print("cl", club)
+    #         return club
+    #     except Club.DoesNotExist:
+    #         raise NotFound("Club not found")
+
+    def perform_create(self, serializer):
+        club_id = self.kwargs.get("club_id")
+        serializer.save(club_id=club_id, writer=self.request.user)
 
 
 class ScheduleViewSet(viewsets.ModelViewSet[Schedule]):
@@ -17,3 +36,11 @@ class ScheduleViewSet(viewsets.ModelViewSet[Schedule]):
     serializer_class = ScheduleSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly,
                           IsWriterOrReadOnly]
+
+    def get_queryset(self):
+        club_id = self.kwargs.get("club_id")
+        return Schedule.objects.filter(club_id=club_id)
+
+    def perform_create(self, serializer):
+        club_id = self.kwargs.get("club_id")
+        serializer.save(club_id=club_id, writer=self.request.user)
