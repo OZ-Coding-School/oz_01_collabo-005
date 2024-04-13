92363195ebaa9c4db79f4dc4c1ff1cd8db76290b
yyysolhhh <solbaram37@gmail.com>
2024-04-13 12:33:50 +0900

    Test: add user test

--- backend/app/user/tests.py
+++ backend/app/user/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class UserModelTests(TestCase):
+    def setUp(self) -> None:
+        self.url = reverse("rest_register")
+
+    def test_create_user_with_email_successful(self) -> None:
+        # Given
+        data = {
+            "email": "user@email.com",
+            "password1": "felsejfla234234",
+            "password2": "felsejfla234234",
+            "nickname": "nick",
+            "nationality": "nationality",
+            "first_name": "firstname",
+            "last_name": "lastname",
+            "phone": "1234",
+            "date_of_birth": "2024-04-01",
+            "profession": "profession",
+        }
+        # When
+        res = self.client.post(self.url, data)
+        # Then
+        self.assertEqual(res.status_code, 201)
