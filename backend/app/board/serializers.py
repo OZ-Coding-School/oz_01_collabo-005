599b42c90ac84e5ce690532e5f0d4d2997eb8476
yyysolhhh <solbaram37@gmail.com>
2024-04-16 12:09:06 +0900

    Feat: add writer, create_at, updated_at to board serializers
    - serializer에 항목 추가

--- backend/app/board/serializers.py
+++ backend/app/board/serializers.py
@@ -9,9 +9,16 @@ from app.board.models import Post, Schedule
 
 
 class PostSerializer(serializers.ModelSerializer[Post]):
+    # writer = serializers.SerializerMethodField("get_writer")
+    writer = serializers.ReadOnlyField(source='writer.nickname')
+
     class Meta:
         model = Post
-        fields = ("title", "content")
+        fields = ("id", "title", "content", "writer", "created_at", "updated_at")
+        read_only_fields = ("writer",)
+
+    # def get_writer(self, model):
+    #     return model.writer.nickname
 
     # def create(self, validated_data):
     #     # validated_data["club"] = self.context["club_id"]
@@ -24,6 +31,8 @@ class PostSerializer(serializers.ModelSerializer[Post]):
 
 
 class ScheduleSerializer(serializers.ModelSerializer[Schedule]):
+    # url = serializers.HyperlinkedIdentityField(view_name="schedule-detail")
+
     class Meta:
         model = Schedule
-        fields = ("title", "content", "event_time", "place", "max_attendees")
+        fields = ("title", "content", "event_time", "place", "max_attendees", "created_at", "updated_at")
