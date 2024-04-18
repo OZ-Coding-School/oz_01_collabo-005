49461c23f100253574344daca73b53e777186b31
최오늘 <healutoday@naver.com>
2024-04-18 23:33:27 +0900

    Style: feedScreen, meethome, mymeet, topnav 레이아웃
    페이지 레이아웃을 하기위해

---
+++ frontend/src/Pages/Meethome/NoticeBoard.tsx
@@ -0,0 +1,44 @@
+import { AiOutlineMore } from "react-icons/ai";
+import "./NoticeBoard.css";
+function NoticeBox() {
+  return (
+    <div className="noticeBoardBox">
+      <div className="writerBox">
+        <div className="writerInfo">
+          <img
+            className="writerPicture"
+            src="https://api.nudge-community.com/attachments/35176"
+            width={46}
+          />
+          <div className="userFeedInfo">
+            <div>포디</div>
+            <ul className="divisionInfo">
+              <li>1일전</li>
+              <li>자유게시판</li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <button className="deleteIcon">
+            <AiOutlineMore size={23} style={{ color: "#000" }} />
+          </button>
+        </div>
+      </div>
+      <div className="joinerFeedBox">
+        안녕하세요 개발자 분들과 소통하고 싶어서 들어왔습니다.
+      </div>
+    </div>
+  );
+}
+
+function NoticeBoard({ button }: { button: React.ReactNode }) {
+  return (
+    <div>
+      <NoticeBox />
+      <NoticeBox />
+      <NoticeBox />
+      {button}
+    </div>
+  );
+}
+export default NoticeBoard;
