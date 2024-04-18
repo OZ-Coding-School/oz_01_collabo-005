f852e7e953807faae3a199cbae7fc23490291065
yyysolhhh <solbaram37@gmail.com>
2024-04-18 12:59:39 +0900

    Fix: admin 페이지에서 SavedPostAdmin 주석처리
    activity/admin.py
    - 글 저장하기는 없어진 기능이기 때문에 admin 필요없음
    
    entrypoint.sh
    - 명령어에 setting 파일 옵션 추가

--- backend/app/activity/admin.py
+++ backend/app/activity/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import JoinedClub, InterestClub, SavedPost
+from .models import JoinedClub, InterestClub
+
 
 # JoinedClub 모델에 대한 관리자 설정
 @admin.register(JoinedClub)
@@ -9,6 +10,7 @@ class JoinedClubAdmin(admin.ModelAdmin):
     search_fields = ('user__username', 'club__name')
     readonly_fields = ('created_at', 'updated_at')
 
+
 # InterestClub 모델에 대한 관리자 설정
 @admin.register(InterestClub)
 class InterestClubAdmin(admin.ModelAdmin):
@@ -17,10 +19,11 @@ class InterestClubAdmin(admin.ModelAdmin):
     search_fields = ('user__username', 'club__name')
     readonly_fields = ('created_at', 'updated_at')
 
+
 # SavedPost 모델에 대한 관리자 설정
-@admin.register(SavedPost)
-class SavedPostAdmin(admin.ModelAdmin):
-    list_display = ('user', 'post', 'created_at')
-    list_filter = ('user', 'post', 'created_at')
-    search_fields = ('user__username', 'post__title')
-    readonly_fields = ('created_at', 'updated_at')
+# @admin.register(SavedPost)
+# class SavedPostAdmin(admin.ModelAdmin):
+#     list_display = ('user', 'post', 'created_at')
+#     list_filter = ('user', 'post', 'created_at')
+#     search_fields = ('user__username', 'post__title')
+#     readonly_fields = ('created_at', 'updated_at')
