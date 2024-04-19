8c6f4e108430d253f190b7465248cf2485b738e9
yyysolhhh <solbaram37@gmail.com>
2024-04-20 01:10:42 +0900

    Feat: pagination 설정
    settings.py
    - rest_framework pagination 사용해서 설정
    - 한 페이지 사이즈 10으로 설정

--- backend/mysite/settings/base.py
+++ backend/mysite/settings/base.py
@@ -187,6 +187,8 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
         "dj_rest_auth.jwt_auth.JWTCookieAuthentication",
     ),
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10
 }
 
 SIMPLE_JWT = {
