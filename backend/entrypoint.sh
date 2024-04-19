9eeabeea2bf50ee83867e4483ea559e959d43171
yyysolhhh <solbaram37@gmail.com>
2024-04-19 22:18:23 +0900

    Chore: entrypoint 오타 수정함

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,8 +4,8 @@
 #exec python manage.py runserver 0.0.0.0:8000
 #python manage.py collectstatic
 
-python manage.py makemigration --settings=mysite.settings.base
-python manage.py migrat --settings=mysite.settings.base
+python manage.py makemigrations --settings=mysite.settings.base
+python manage.py migrate --settings=mysite.settings.base
 python manage.py runserver 0.0.0.0:80 --settings=mysite.settings.base
 #python manage.py runserver
 #gunicorn --bind 0:8000 mysite.wsgi:application
